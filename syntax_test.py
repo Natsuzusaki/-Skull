927188fc85699dd3b00fb48a2d17ac98f54988b9
Naer <natsuzusakii@gmail.com>
2025-09-02 21:55:57 +0800

    a

--- syntax_test.py
+++ syntax_test.py
@@ -6,14 +6,11 @@ func("ah")
 #nigga
 #yawa test
 #???
-<<<<<<< HEAD
+
 #nigggaaaaaaaaa
 #update2
-=======
-<<<<<<< HEAD
+
 #!!
 #aaaaaaaaaaaaaaaaaaa
-=======
+
 #nigggaaaaaaaaa
->>>>>>> 6ecc4dd4814b76a175c66511273397358929719e
->>>>>>> a576e16c6aee499864c20a72ea8a43ab556e8d7f
